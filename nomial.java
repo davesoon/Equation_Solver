6f076fd0a177ca8e9d4a1c8e16c25b644a70bc5f
davesoon <the.david.sun@gmail.com>
2014-04-14 13:21:23 -0400

    Create nomial.java

---
+++ nomial.java
@@ -0,0 +1,15 @@
+class Nomial{
+  boolean isPositive;
+  float coefficient;
+  int power;
+  
+  Nomial(){
+    this(true, 0.0, 0);
+  }
+  
+  Nomial(boolean p, float c, power p){
+    isPositive = p;
+    coefficent = c;
+    power = p;
+  }
+}
