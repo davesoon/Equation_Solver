28eeeffb25d4758b2bab5e75ae5e9654a3bb66b7
davesoon <the.david.sun@gmail.com>
2014-04-15 08:52:40 -0400

    Update and rename InputThree.java to EquationSolver.java

---
+++ EquationSolver.java
@@ -0,0 +1,77 @@
+/**
+ * its 4-12-14 and I am writing this poor code
+ * its fun time
+ * 11:00 PM
+ * rgw3d
+ */
+import java.util.Scanner;
+public class EquationSolver{
+    
+    public static void main(String args[]){
+        
+        System.out.println("Input your equation");
+        String originalInput=simplifyInput(new Scanner(System.in).nextLine());//get and simplify the input
+
+        //so i need to set up a pemdas system.  the parenthesis can call the rest of it 
+        //and so on.  like recurson 
+        //yay
+        //fuck
+        
+        /*leftSide=Functions.parenthesis(leftSide);
+        leftSide=Functions.exponent(leftSide);
+        leftSide=Functions.multiplication(leftSide);
+        leftSide=Functions.addition(leftSide);
+        
+        //I think this part is self explanatory 
+        
+        rightSide=Functions.parenthesis(rightSide);
+        rightSide=Functions.exponent(rightSide);
+        rightSide=Functions.multiplication(rightSide);
+        rightSide=Functions.addition(rightSide);
+        
+        */
+    }
+    
+    public static boolean isEquation(String string){
+        simplifyInput(string);
+        
+        if(string.indexOf(string.indexOf(=)))
+            return false;
+    }
+    
+    public static String simplifyInput(String fix){
+        if(!(fix.contains("=")))//it needs to have a equals sign
+            fix=fix+"=0";
+            
+        fix=fix.replace(" ","");//space is bad
+        
+        fix=fix.replace("--","+"); //minus a minus is addition.  make it simple
+        
+        fix=fix.replace("-","+-");  //replace a negative with just plus a minus.  makes it easier
+        
+        //this will be updated later as I fix all the syntax errors that come with exponents and parentheses
+        return fix;
+        
+    }
+    
+    public static ArrayList toArrayList(String equation){
+        ArrayList newEquation = new ArrayList();
+        simplifyInput(equation);
+        for(int i = 0; i < equation.length; i ++){
+                switch(equation.charAt(i)){
+                    
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                    //case '%'://I don't know if I'll implement this
+                        newEquation.add(equation.charAt(i));
+                    break;
+                    
+                    default:
+                    break;
+                }
+        }
+    }
+    
+}
